868f40fa5bbef0c81b366bfa92099c547f06e9fb
Michael Peterson <michaeljoepeterson@gmail.com>
2020-05-24 13:41:39 -0600

    initial setup for get jobs ajax req;

--- job-lists/job-list-1/job-list.js
+++ job-lists/job-list-1/job-list.js
@@ -1,7 +1,50 @@
-function JobList_1(){
+function JobList_1(options){
+    this.constructor(options);
+}
 
+JobList_1.prototype.initListeners = function(){
+    
+}
+//placeholder for ajax calls
+JobList_1.prototype.getJobs = function(){
+    let req = {
+        method:'GET',
+        url:'https://job-dummy-api.herokuapp.com/api/indeed'
+    };
+
+    $.ajax(req)
+
+    .then(response => {
+        console.log(response);
+    })
+
+    .catch(err => {
+        console.log('Error getting data: ',err);
+    })
 }
 
 JobList_1.prototype.constructor = function(options){
-    
+    this.title = "Simple List";
+    var parentSelector = options.parentClass.startsWith('.') ? options.parentClass : '.' + options.parentClass;
+    this.parent = $(parentSelector)[0];
+    console.log(this.parent);
 }
+
+JobList_1.prototype.render = function(){
+    console.log(this.title);
+    this.getJobs();
+}
+
+function init(){
+    var jobptions = {
+        parentClass:'job-card'
+    };
+
+    var joblist = new JobList_1(jobptions);
+
+    joblist.render();
+}
+
+$(document).ready(function(){
+    init();
+})
