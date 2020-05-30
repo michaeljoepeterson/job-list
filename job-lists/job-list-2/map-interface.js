b36473ad4acb63427f5c70df0c00dc2c3c917510
Michael Peterson <michaeljoepeterson@gmail.com>
2020-05-30 15:49:55 -0600

    update map to use first job for center

--- job-lists/job-list-2/map-interface.js
+++ job-lists/job-list-2/map-interface.js
@@ -19,11 +19,11 @@ MapInterface.prototype.getLocationData = function(data){
 
 MapInterface.prototype.getInfoData = function(data){
 
-}
+};
 
 MapInterface.prototype.createInfo = function(locations){
     var locations = locations ? locations : this.locations;
-}
+};
 
 MapInterface.prototype.addMarkers = function(locations){
     var locations = locations ? locations : this.locations;
@@ -31,18 +31,28 @@ MapInterface.prototype.addMarkers = function(locations){
     for(var i = 0;i < locations.length;i++){
         var pos = locations[i];
         var marker = new google.maps.Marker({position: pos, map: this.map});
+        this.markers.push(marker);
     }
 
-}
+};
 
 MapInterface.prototype.createMap = function(){
     var mapOptions = {
-        center: {lat: 53.485450, lng: -113.488751},
         zoom: this.defaultZoom
     };
     this.map = new google.maps.Map(this.mapElement,mapOptions);
     this.addMarkers();
-}
+    if(this.jobData && this.jobData.length > 0){
+        this.positionMap(this.jobData[0]);
+    }
+    else{
+        this.map.setcenter({
+            lat:53.485450, 
+            lng:-113.488751
+        });
+    }
+    
+};
 
 MapInterface.prototype.positionMap = function(job){
     var zoom = 15;
@@ -53,7 +63,7 @@ MapInterface.prototype.positionMap = function(job){
 
     this.map.setCenter(center);
     this.map.setZoom(zoom);
-}
+};
 
 MapInterface.prototype.constructor = function(options){
     this.mapElement = document.getElementById(options.id);
@@ -61,21 +71,9 @@ MapInterface.prototype.constructor = function(options){
     this.jobData = options.jobData;
     this.defaultZoom = 13;
     var locations = this.jobData ? this.getLocationData(this.jobData) : [];
-    /*this.locations = options.locations ? options.locations : [
-        {
-            lat:53.485450, 
-            lng:-113.488751
-        },
-        {
-            lat:53.477103, 
-            lng:-113.488310
-        },
-        {
-            lat:53.547536, 
-            lng:-113.492812
-        }
-    ]
-    */
+
     this.locations = locations;
+    this.infoWindows = [];
+    this.markers = [];
     this.createMap();
-}
+};
